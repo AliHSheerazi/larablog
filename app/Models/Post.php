4c94d6bfbb3bbd8e6bbcb3ca859ea61d24b64636
AliHSheerazi <101172736+AliHSheerazi@users.noreply.github.com>
2022-04-18 22:40:44 +0500

    New post controller and update model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected $table = 'posts';
+
+    protected $fillable = [
+        'name',
+        'category_id',
+        'created_by',
+        'slug',
+        'description',
+        'yt_iframe',
+        'meta_description',
+        'meta_title',
+        'meta_keyword',
+        'status',
+    ]
 }
