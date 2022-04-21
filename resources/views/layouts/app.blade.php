a2d94ef13989646b1c04e15561494c70ca58f942
AliHSheerazi <101172736+AliHSheerazi@users.noreply.github.com>
2022-04-21 21:06:20 +0500

    Starting of Frontend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,7 +80,9 @@
         </main>
     </div>
 
+    <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}" crossorigin="anonymous"></script>
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}" crossorigin="anonymous"></script>
+    
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/',[App\http\Controllers\Frontend\FrontendController::class, 'index']);
 
 Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
     Route::get('/dashboard',[App\http\Controllers\Admin\DashboardController::class, 'index']);
